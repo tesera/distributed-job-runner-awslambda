1f4b2497ea67110d7b31aa8a4cc191c3ca830fd3
zmpeg <zandstra.matt@gmail.com>
2015-11-30 10:13:41 -0700

    fix up deploy function

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< Updated upstream
-# desc:     zips up lambda function resources and uploads to aws
-# usage:    ./scripts/deploy.sh
-# docs:     http://docs.aws.amazon.com/cli/latest/reference/lambda/upload-function.html
-
-zip -r function.zip package.json node_modules/* lib/* index.js .env
-
-aws lambda update-function-code \
-    --function-name shelljob \
-    --zip-file fileb://function.zip
-=======
-# desc:   zips up lambda function resources and uploads to aws
-# docs:   http://docs.aws.amazon.com/cli/latest/reference/lambda/upload-function.html
-
 zip -r function.zip package.json node_modules/* lib/* index.js
 
 aws lambda update-function-code \
@@ -26,8 +12,5 @@ aws lambda update-function-code \
  --timeout 60  \
  --memory-size 256  \
  --region us-east-1
-
-# aws lambda update-function-code --function-name q2worker --zip-file fileb://function.zip
->>>>>>> Stashed changes
 
 rm function.zip
