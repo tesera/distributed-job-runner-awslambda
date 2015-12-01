387fd5d6cccd6d4161227578f799c3730f193d7c
zmpeg <zandstra.matt@gmail.com>
2015-12-01 11:36:34 -0700

    should use sudo

--- client/client.js
+++ client/client.js
@@ -22,7 +22,9 @@ winston.log('info', 'client: forked with PID: '+process.pid);
 function shutdown() {
     winston.log('info', 'client: djr ' + instanceId + ' is shutting down');
     if(process.env.NODE_ENV !== 'development') {
-        spawn('bash', ['shutdown -h now']);
+        var shutoff = spawn('sudo ', ['shutdown','-h','now']);
+        shutoff.on('data', function(data) { winston.log('info', 'client: shutdown: '+data); });
+        shutoff.on('close', function() { winston.log('info', 'client: shutdown ok'); });
     } else {
         winston.log('info', 'client: not shutting down since this is development');
     }
