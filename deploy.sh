8bbe7b5a2f4dfdcb5c320eaea8ed5808bd433c79
zmpeg <zandstra.matt@gmail.com>
2015-12-01 08:13:08 -0700

    clean up deploy, exec bit on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
+
+PWD=`pwd`
+
 zip -r function.zip package.json node_modules/* lib/* index.js
 
 aws lambda update-function-code \
- --function-name q2worker \
- --function-zip function.zip  \
- --runtime nodejs  \
- --role arn:aws:iam::674223647607:role/lambda_exec_shelljob \
- --handler index.handler  \
- --mode event  \
- --timeout 60  \
- --memory-size 256  \
+ --function-name distributed-job-runner \
+ --zip-file fileb://$PWD/function.zip \
  --region us-east-1
 
 rm function.zip
